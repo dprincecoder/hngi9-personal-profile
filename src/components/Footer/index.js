ae6c4e3015aba2b69972ca82bbd8eaa8588cbb92
dprincecoder <dprincecoder@gmail.com>
2022-11-03 20:15:11 +0100

    refactored code base

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,9 +4,9 @@ import "./footer.css";
 const index = () => {
   return (
     <footer>
-      <img src="zuri-icon.svg" alt="" className="zuri" />
+      <img src="assets/zuri-icon.svg" alt="" className="zuri" />
       <p className="sample-txt"> HNG Internship 9 Frontend Task</p>
-      <img src="i4g-icon.svg" alt="" className="hng" />
+      <img src="assets/i4g-icon.svg" alt="" className="hng" />
     </footer>
   );
 };
